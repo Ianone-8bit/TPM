52477a5216f1765a1449a04a935d4a76c94cb14a
widagda <ghivasatriawidagda@gmail.com>
2026-03-07 19:24:39 +0700

    ganti cara login aja dikit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'pages/login_page.dart';
 
 void main() {
   runApp(const MainApp());
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool isloggedin = false;
+
+  final Map<String, String> users = {
+    "wdagda": "##beltrayal2",
+    "gipa": "209",
+    "orel": "225",
+    "kirana": "227",
+    "driessen": "229",
+    "woo": "nyawit",
+  };
+
+  void _login() {
+    final String username = _usernameController.text.trim();
+    final String password = _passwordController.text.trim();
+
+    if (users[username] == password) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Login Berhasil"),
+          backgroundColor: Colors.green,
+        ),
+      );
+      setState(() {
+        isloggedin = true;
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Login gagal"),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Login CUYYY',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login cuyy',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                label: Text("Username"),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                label: Text("Password"),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _login,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                minimumSize: Size(250, 60),
+              ),
+              child: Text("Login"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
